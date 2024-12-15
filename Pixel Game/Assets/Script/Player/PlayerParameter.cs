cac5621e203f16749dc7f19602d53e343a68d97e
lastre1208 <lastobserver.1208@gmsail.com>
2024-12-15 22:35:12 +0900

    レベルアップUIの実装

--- Pixel Game/Assets/Script/Player/PlayerParameter.cs
+++ Pixel Game/Assets/Script/Player/PlayerParameter.cs
@@ -7,6 +7,7 @@ using UnityEngine.Rendering;
 
 public class PlayerParameter: MonoBehaviour
 {
+    [SerializeField] LevelUp levelUp;
     public CommonParameter Common_P;
     public float Exp;//経験値
     public float Speed;//速度
@@ -14,6 +15,8 @@ public class PlayerParameter: MonoBehaviour
     public float Lv;//レベル
     public float N_Lv;//次のレベルに必要な経験値
     public float Lv_Rate;
+    public float SkillPoint;//各強化に用いるスキルポイント
+    public float GetSP;//獲得できるスキルポイント
     private void Update()
     {
         if (Exp > N_Lv)
@@ -22,8 +25,11 @@ public class PlayerParameter: MonoBehaviour
             {
                 Lv++;
                 N_Lv *= Lv_Rate;
+                SkillPoint += GetSP;
+                GetSP+=Lv;
             }
             Exp = 0;
+            levelUp.OpenMenu();
         }
       
     }
