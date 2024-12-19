a692ea6807c0f91393f74fc614ced81af50b3acd
lastre1208 <lastobserver.1208@gmsail.com>
2024-12-20 02:06:10 +0900

    プレイヤー自身のダメージ＆無敵処理の実装
    続いて一部の変数を外部から取得する事で複数のピクセルの無敵時間を共有するようにする。

--- Pixel Game/Assets/Script/Attack/HitPlayer.cs
+++ Pixel Game/Assets/Script/Attack/HitPlayer.cs
@@ -1,18 +1,63 @@
-using System.Collections;
-using System.Collections.Generic;
+// 無敵状態を管理するクラス
 using UnityEngine;
-using UnityEngine.Events;
+
+
 public class HitPlayer : Damage
 {
-    // Start is called before the first frame update
-  
-    
-    // Update is called once per frame
+    [SerializeField] PlayerParameter player;  // プレイヤーのパラメーター
+    private EnemyParameter enemy;
+    private float Hitcount;
+    private bool Hit=true;
+    private void Start()
+    {
+        if (player == null)
+        {
+            player = GameObject.FindWithTag("P_Status").GetComponent<PlayerParameter>();
+        }
+    }
+
+    private void Update()
+    {
+        // 無敵時間を更新（プレイヤー以外のオブジェクトも共有しているのでここでカウント）
+        if (!Hit)
+        {
+            Hitcount += Time.deltaTime;
+            if (Hitcount > player.DamageTime)
+            {
+                Hit = true;
+                Hitcount = 0;
+            }
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Enemy") ) {
-           
-            Debug.Log(collision.name);
+        // 無敵状態中はダメージを受けない
+        if (!Hit)
+            return;
+
+        if (collision.CompareTag("Enemy") && gameObject.tag==("Player"))
+        {
+            enemy = collision.GetComponent<EnemyParameter>();
+            if (enemy != null)
+            {
+                if (player != null)
+                {
+                    // ダメージ処理
+                    DamageEvent(player.Common_P, enemy.Common_E);
+                    Debug.Log(gameObject.name);
+                    // 無敵を開始
+                    Hit= false;
+                }
+                else
+                {
+                    Debug.Log("Player not found");
+                }
+            }
+            else
+            {
+                Debug.Log("Enemy parameter not found");
+            }
         }
     }
 }
